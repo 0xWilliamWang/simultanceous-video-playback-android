743979861b4079e3c557bfe0e3893d09b690aae0
wangkaixuan <2654189525@qq.com>
2021-07-24 22:57:53 +0800

    add login ui

--- app/src/main/java/com/example/practiceandroid/MainActivity.kt
+++ app/src/main/java/com/example/practiceandroid/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.practiceandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+        val loginBtn:Button = findViewById(R.id.login)
+        loginBtn.setOnClickListener(
+            {Toast.makeText(this,"正在开发中",Toast.LENGTH_SHORT).show()}
+        )
     }
 }
