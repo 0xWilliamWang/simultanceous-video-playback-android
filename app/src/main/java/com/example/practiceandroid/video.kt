73de4e3c3dfdf8ec99086a226be01d38c19df4ab
wangkaixuan <2654189525@qq.com>
2021-07-25 14:23:11 +0800

    support play video from raw

--- app/src/main/java/com/example/practiceandroid/video.kt
+++ app/src/main/java/com/example/practiceandroid/video.kt
@@ -1,19 +1,39 @@
 package com.example.practiceandroid
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import com.example.practiceandroid.databinding.LoginBinding
 import com.example.practiceandroid.databinding.VideoBinding
 
 class video : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding = VideoBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.videoStart.setOnClickListener({
-            Log.d("log","you click videoStart")
+        var vb = VideoBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+        var uri = Uri.parse("android.resource://$packageName/${R.raw.video}")
+        vb.videoView.setVideoURI(uri)
+
+        vb.videoStart.setOnClickListener({
+            Log.d("log","you click start")
+            if (!vb.videoView.isPlaying){
+                vb.videoView.start()
+            }
         })
+
+        vb.videoPause.setOnClickListener({
+            Log.d("log","you click pause")
+            if (vb.videoView.isPlaying){
+                vb.videoView.pause()
+            }
+        })
+
+        vb.videoResume.setOnClickListener({
+            Log.d("log","you click resume")
+            if (vb.videoView.isPlaying){
+                vb.videoView.resume()
+            }
+        })
+
     }
 }
