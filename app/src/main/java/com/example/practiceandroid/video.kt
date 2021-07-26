d13a760a1568c6d41aff49f8b224cae3efc23169
wangkaixuan <2654189525@qq.com>
2021-07-26 23:21:47 +0800

    play the selectd video

--- app/src/main/java/com/example/practiceandroid/video.kt
+++ app/src/main/java/com/example/practiceandroid/video.kt
@@ -1,36 +1,56 @@
 package com.example.practiceandroid
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.practiceandroid.databinding.VideoBinding
 
 class video : AppCompatActivity() {
+    val REQUEST_IMAGE_GET = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var vb = VideoBinding.inflate(layoutInflater)
         setContentView(vb.root)
-        var uri = Uri.parse("android.resource://$packageName/${R.raw.video}")
-        vb.videoView.setVideoURI(uri)
+        var videoSelectLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(), { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Log.d("log", "file:${result.data?.data}")
+                vb.videoView.setVideoURI(result.data?.data)
+            }
+        })
+
+        vb.videoSelect.setOnClickListener({
+            Log.d("log", "you click select")
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "video/*"
+            }
+
+            if (intent.resolveActivity(packageManager) != null) {
+                videoSelectLauncher.launch(intent)
+            }
+        })
 
         vb.videoStart.setOnClickListener({
-            Log.d("log","you click start")
-            if (!vb.videoView.isPlaying){
+            Log.d("log", "you click start")
+            if (!vb.videoView.isPlaying) {
                 vb.videoView.start()
             }
         })
 
         vb.videoPause.setOnClickListener({
-            Log.d("log","you click pause")
-            if (vb.videoView.isPlaying){
+            Log.d("log", "you click pause")
+            if (vb.videoView.isPlaying) {
                 vb.videoView.pause()
             }
         })
 
         vb.videoResume.setOnClickListener({
-            Log.d("log","you click resume")
-            if (vb.videoView.isPlaying){
+            Log.d("log", "you click resume")
+            if (vb.videoView.isPlaying) {
                 vb.videoView.resume()
             }
         })
