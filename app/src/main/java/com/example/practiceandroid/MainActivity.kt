24a0bea7c83f309698650d8fa582ea144b7dc08f
wangkaixuan <2654189525@qq.com>
2021-08-05 00:36:50 +0800

    call server api

--- app/src/main/java/com/example/practiceandroid/MainActivity.kt
+++ app/src/main/java/com/example/practiceandroid/MainActivity.kt
@@ -6,8 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.practiceandroid.databinding.LoginBinding
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.*
 import java.lang.Exception
 import java.lang.StringBuilder
 import java.net.HttpURLConnection
@@ -16,17 +15,26 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: LoginBinding
-    private fun sendHttp() {
+    private fun loginVerify() {
         thread {
             var conn: HttpURLConnection? = null
             try {
-                var response = StringBuilder()
-                var url = URL("http://blog.wangkaixuan.com/technical/")
+                val response = StringBuilder()
+                val url = URL("http://192.168.50.174:18605/login/")
                 conn = url.openConnection() as HttpURLConnection
-                conn.requestMethod = "GET"
+                conn.requestMethod = "POST"
                 conn.connectTimeout = 8000
                 conn.readTimeout = 8000
-                var input = conn.inputStream
+                conn.setRequestProperty("Content-Type","application/json")
+                conn.setRequestProperty("Accept","application/json")
+                val out:OutputStream = conn.outputStream
+                val writer = BufferedWriter(OutputStreamWriter(out,"UTF-8"))
+                writer.write("{\"account\":\"WangKaixuan1\",\"password\":\"wkxpassword\"}")
+                writer.flush()
+                writer.close()
+                out.close()
+
+                val input = conn.inputStream
                 val reader = BufferedReader(InputStreamReader(input))
                 reader.use {
                     reader.forEachLine {
@@ -34,6 +42,13 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 Log.d("http", response.toString())
+
+                if (binding.account.text.toString() == "wkx" && binding.password.text.toString() == "123") {
+                    val intent = Intent(this, video::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "账户或密码错误，请重试", Toast.LENGTH_SHORT).show()
+                }
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
@@ -47,17 +62,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         var binding = LoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.send.setOnClickListener {
-            Log.d("log", "sendHttp")
-        }
-        binding.login.setOnClickListener {
 
-            if (binding.account.text.toString() == "wkx" && binding.password.text.toString() == "123") {
-                val intent = Intent(this, video::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "账户或密码错误，请重试", Toast.LENGTH_SHORT).show()
-            }
+        binding.login.setOnClickListener {
+            loginVerify()
         }
     }
 }
