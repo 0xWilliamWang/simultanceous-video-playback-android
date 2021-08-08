9e81d5d802ea8fe7e1a5daa3527ab523fff04e0c
iroan <2654189525@qq.com>
2021-08-08 13:12:31 +0800

    format

--- app/src/main/java/com/example/practiceandroid/video.kt
+++ app/src/main/java/com/example/practiceandroid/video.kt
@@ -9,28 +9,26 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.example.practiceandroid.databinding.VideoBinding
 
 class video : AppCompatActivity() {
-    val REQUEST_IMAGE_GET = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var vb = VideoBinding.inflate(layoutInflater)
+        val vb = VideoBinding.inflate(layoutInflater)
         setContentView(vb.root)
-        var videoSelectLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(), { result ->
+        val videoSelectLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 Log.d("log", "file:${result.data?.data}")
                 vb.videoView.setVideoURI(result.data?.data)
             }
-        })
+        }
 
-        vb.forDebug.setOnClickListener({
+        vb.forDebug.setOnClickListener {
             Log.d("log", "you click forDebug")
             Thread(Runnable {
                 val sql = "select * from comm.eth_miner limit 3"
                 Log.d("log", sql)
             }).start()
-        })
+        }
 
-        vb.videoSelect.setOnClickListener({
+        vb.videoSelect.setOnClickListener {
             Log.d("log", "you click select")
             val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
                 type = "video/*"
@@ -39,28 +37,28 @@ class video : AppCompatActivity() {
             if (intent.resolveActivity(packageManager) != null) {
                 videoSelectLauncher.launch(intent)
             }
-        })
+        }
 
-        vb.videoStart.setOnClickListener({
+        vb.videoStart.setOnClickListener {
             Log.d("log", "you click start")
             if (!vb.videoView.isPlaying) {
                 vb.videoView.start()
             }
-        })
+        }
 
-        vb.videoPause.setOnClickListener({
+        vb.videoPause.setOnClickListener {
             Log.d("log", "you click pause")
             if (vb.videoView.isPlaying) {
                 vb.videoView.pause()
             }
-        })
+        }
 
-        vb.videoResume.setOnClickListener({
+        vb.videoResume.setOnClickListener {
             Log.d("log", "you click resume")
             if (vb.videoView.isPlaying) {
                 vb.videoView.resume()
             }
-        })
+        }
 
 
     }
